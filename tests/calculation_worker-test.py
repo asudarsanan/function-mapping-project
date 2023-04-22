1da9c8e4e3cbc41cfb7c75bfb0585e45b0cc391d
Aasish Sudarsanan <asudarsanan@protonmail.com>
2023-04-23 00:18:31 +0530

    revised testing suits

--- tests/calculation_worker-test.py
+++ tests/calculation_worker-test.py
@@ -1,33 +1,46 @@
 import unittest
 from unittest.mock import MagicMock
-from function_model_worker import IdealFunction, CoreFunction
-from calculations_worker import * 
-from main import minimiseLoss, errorSquared
 import pandas as pd
 
+from calculations_worker import errorSquared
+from function_model_worker import CoreFunction
+from main import minimiseLoss
+
+
 class UnitTest(unittest.TestCase):
+    """
+    Unit tests for errorSquared function.
+    """
     def setUp(self):
-        # Setting up dummy data
-        firstDataSet = {"x":[5.0,7.0,9.0],"y":[10.0,11.0,12.0]}
-        secondDataSet = {"x":[1.0,2.0,3.0], "y":[7.0,8.0,9.0]}
+        """
+        Setting up dummy data and CoreFunction instances.
+        """
+        first_data_set = {"x": [5.0, 7.0, 9.0], "y": [10.0, 11.0, 12.0]}
+        second_data_set = {"x": [1.0, 2.0, 3.0], "y": [7.0, 8.0, 9.0]}
 
-        self.firstDataFrame = pd.DataFrame(data=firstDataSet)
-        self.secondDataframe = pd.DataFrame(data=secondDataSet)
+        self.first_dataframe = pd.DataFrame(data=first_data_set)
+        self.second_dataframe = pd.DataFrame(data=second_data_set)
 
-        self.firstFunction = CoreFunction('')
-        self.firstFunction.dataframe = self.firstDataFrame
-        self.secondFunction = CoreFunction('')
-        self.secondFunction.dataframe = self.secondDataframe
-
+        self.first_function = CoreFunction('')
+        self.first_function.dataframe = self.first_dataframe
+        self.second_function = CoreFunction('')
+        self.second_function.dataframe = self.second_dataframe
 
     def tearDown(self):
+        """
+        Clean up after each test.
+        """
         pass
 
-    def testErrorSquared(self):
-        # check to see first function correct value
-        self.assertEqual(errorSquared(self.firstFunction, self.secondFunction), 15.0)
-        # check to test the loss function value
-        self.assertEqual(errorSquared(self.secondFunction, self.firstFunction), 14.0)
-        # test to check that regression is zero or not
-        self.assertEqual(errorSquared(self.firstFunction, self.firstFunction), 0.0)
+    def test_error_squared(self):
+        """
+        Test errorSquared function with different CoreFunction instances.
+        """
+        # Check to see the correct value for the first function
+        self.assertEqual(errorSquared(self.first_function, self.second_function), 15.0)
 
+        # Check to test the loss function value
+        self.assertEqual(errorSquared(self.second_function, self.first_function), 14.0)
+
+        # Test to check that regression is zero or not
+        self.assertEqual(errorSquared(self.first_function, self.first_function), 0.0)
